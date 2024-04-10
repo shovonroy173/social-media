e0fc80d61e40abef0f88c52a42f09df4d238e7b6
shovonroy173 <shovonroy2003@gmail.com>
2024-04-10 09:24:50 +0530

    modal added

---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./modal.css";
+const Modal = () => {
+  return (
+    <div className="modalContainer">
+      <div className="modalContent">Modal</div>
+    </div>
+  );
+};
+
+export default Modal;
