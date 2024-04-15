03385bac4921aad6fd991657bcd24d7455863b12
shovonroy173 <shovonroy2003@gmail.com>
2024-04-15 22:50:26 +0530

    register page modified

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -5,18 +5,18 @@ import { useState } from "react";
 
 export default function Register() {
   const dispatch = useDispatch();
-  const [name , setName] = useState("");
-  const [email , setEmail] = useState("");
-  const [password , setPassword] = useState("");
-  const [passwordAgain , setPasswordAgain] = useState("");
-  const handleClick = ()=>{
-    if(password !== passwordAgain) {return alert('Passwords do not match');}
-    else{
-      register(dispatch , {name , email , password });
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordAgain, setPasswordAgain] = useState("");
+  const handleClick = () => {
+    if (password !== passwordAgain) {
+      return alert("Passwords do not match");
+    } else {
+      register(dispatch, { name, email, password });
     }
-    
-  }
-  console.log("LINE AT 14" , name , password , email);
+  };
+  console.log("LINE AT 14", name, password, email);
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -28,13 +28,31 @@ export default function Register() {
         </div>
         <div className="loginRight">
           <div className="loginBox">
-            <input placeholder="Username" className="loginInput" onChange={((e)=>(setName(e.target.value)))} />
-            <input placeholder="Email" className="loginInput" onChange={((e)=>(setEmail(e.target.value)))} />
-            <input placeholder="Password" type="password" className="loginInput" onChange={((e)=>(setPassword(e.target.value)))} />
-            <input placeholder="Confirm Password" className="loginInput" type="password" onChange={((e)=>(setPasswordAgain(e.target.value)))} />
-            <input placeholder="Confirm Password" className="loginInput" type="password" onChange={((e)=>(setPasswordAgain(e.target.value)))} />
-            <button className="loginButton" onClick={handleClick}>Sign Up</button>
-
+            <input
+              placeholder="Username"
+              className="loginInput"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              placeholder="Email"
+              className="loginInput"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              placeholder="Password"
+              type="password"
+              className="loginInput"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <input
+              placeholder="Confirm Password"
+              className="loginInput"
+              type="password"
+              onChange={(e) => setPasswordAgain(e.target.value)}
+            />
+            <button className="loginButton" onClick={handleClick}>
+              Sign Up
+            </button>
           </div>
         </div>
       </div>
