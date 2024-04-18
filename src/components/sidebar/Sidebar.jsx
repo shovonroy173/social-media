17d60c08058fe042ec933c33a399762a9aebfcc6
shovonroy173 <shovonroy2003@gmail.com>
2024-04-18 21:16:47 +0530

    sidebar modified

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,37 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import "./sidebar.css"
-import {RssFeed , Chat , PlayCircleFilledOutlined,
+import React, { useEffect, useState } from "react";
+import "./sidebar.css";
+import {
+  RssFeed,
+  Chat,
+  PlayCircleFilledOutlined,
   Group,
   Bookmark,
   HelpOutline,
   WorkOutline,
   Event,
-  School,} from '@mui/icons-material';
+  School,
+} from "@mui/icons-material";
 
-import Closefriend from '../closefriend/Closefriend';
+import Closefriend from "../closefriend/Closefriend";
 import axios from "axios";
+import { useSelector } from "react-redux";
+import {useNavigate} from "react-router-dom";
 
-const Sidebar = () => { 
-  // const user = useSelector((state)=>(state.user));
-  // console.log("LINE AT 17" , user);
-  // const [closeFriendsData , setCloseFriendsData] = useState({});
-  // useEffect(()=>{
-  //   const getCloseFriend = async()=>{
-      
-  //     try {
-  //       const closeFriends = await axios.get(`http://localhost:5000/api/user/friends/${user.currentUser?._id}`);
-  //       setCloseFriendsData(closeFriends.data);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
-  //   getCloseFriend();
-  // } , [user.currentUser._id]);
+const Sidebar = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  console.log("LINE AT 21", currentUser?._id);
+  const [closeFriendsData, setCloseFriendsData] = useState([]);
+  useEffect(() => {
+    const getCloseFriend = async () => {
+      try {
+        const res = await axios.get(
+          `http://localhost:5000/api/user/friends/${currentUser?._id}`
+        );
+        // console.log(res.data);
+        setCloseFriendsData(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getCloseFriend();
+  }, [currentUser._id]);
 
-  const [users , setUsers] = useState([]);
-  useEffect(()=>{
-    const getCloseFriend = async()=>{
-      
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const getUsers = async () => {
       try {
         const users = await axios.get(`http://localhost:5000/api/user/`);
         setUsers(users.data);
@@ -39,60 +46,56 @@ const Sidebar = () => {
         console.log(error);
       }
     };
-    getCloseFriend();
-  } , []);
-  // console.log("LINE AT 31"  , users);
+    getUsers();
+  }, []);
+  const currentDate = new Date();
+  const newYear = currentDate.getFullYear();
+  const newMonth = currentDate.getMonth();
+  const newDate = currentDate.getDate();
+  console.log(newYear , newMonth , newDate);
+  const bUsers = users.filter((item) => {
+    const bDay = new Date(item.birthday);
+    const year = bDay.getFullYear();
+    const month = bDay.getMonth();
+    const date = bDay.getDate();
+    console.log(year , month , date);
+    return year === newYear && month === newMonth && date === newDate;
+  });
+const navigate = useNavigate()
+  console.log("LINE AT 50", bUsers);
   return (
-    <div className='sidebar'>
+    <div className="sidebar">
       <div className="sidebarWrapper">
-      <ul className="sidebarList">
-          <li className="sidebarListItem">
-            <RssFeed className="sidebarIcon" />
-            <span className="sidebarListItemText">Feed</span>
-          </li>
-          <li className="sidebarListItem">
-            <Chat className="sidebarIcon" />
-            <span className="sidebarListItemText">Chats</span>
-          </li>
-          <li className="sidebarListItem">
-            <PlayCircleFilledOutlined className="sidebarIcon" />
-            <span className="sidebarListItemText">Videos</span>
-          </li>
-          <li className="sidebarListItem">
-            <Group className="sidebarIcon" />
-            <span className="sidebarListItemText">Groups</span>
-          </li>
-          <li className="sidebarListItem">
-            <Bookmark className="sidebarIcon" />
-            <span className="sidebarListItemText">Bookmarks</span>
-          </li>
-          <li className="sidebarListItem">
-            <HelpOutline className="sidebarIcon" />
-            <span className="sidebarListItemText">Questions</span>
-          </li>
-          <li className="sidebarListItem">
-            <WorkOutline className="sidebarIcon" />
-            <span className="sidebarListItemText">Jobs</span>
-          </li>
-          <li className="sidebarListItem">
-            <Event className="sidebarIcon" />
-            <span className="sidebarListItemText">Events</span>
-          </li>
-          <li className="sidebarListItem">
-            <School className="sidebarIcon" />
-            <span className="sidebarListItemText">Courses</span>
-          </li>
-        </ul>
-        <button className="sidebarButton">Show More</button>
+        <div className="birthdayContainer">
+          <img src={"/assets/gift.png"} alt="" className="birthdayImg" />
+          <div className="birthdayUsers">
+            {bUsers.length !== 0 ? 
+            <>
+              {bUsers.map((item , index)=>(
+              <b onClick={()=> navigate(`/profile/${item?._id}`)} key={index} className="birthdayUsersName">{item.name} , </b>
+            ))}
+            <>has birthday today</>
+            </> 
+            : "No one has a birthday today!"}
+            
+          </div>
+        </div>
         <hr className="sidebarHr" />
-        <ul className='sidebarFriendList'>
-          {users.map((u)=>(
-            <Closefriend key={u._id} user={u}/>
-          ))}
+        <div className="sidebarFriendsText">Friends</div>
+        <ul className="sidebarFriendList">
+          {closeFriendsData ? (
+            <>
+              {closeFriendsData?.map((u) => (
+                <Closefriend key={u._id} user={u} />
+              ))}
+            </>
+          ) : (
+            "No friends till now!"
+          )}
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
