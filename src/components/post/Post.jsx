e8393933220e76668ae57c98e7244588cfdf70e3
shovonroy173 <shovonroy2003@gmail.com>
2024-04-30 18:08:26 +0530

    modi: post update

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,64 +1,100 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import "./post.css";
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import axios from 'axios';
-import {useDispatch, useSelector} from "react-redux";
-import { like } from '../../redux/apiCalls';
-const Post = ({post}) => {
-  const localPost = useSelector(((state)=>(state.post))); 
-  // console.log(localPost.currentPost);
-  const [creator , setCreator] = useState("");
-  const [loggedUserId , setLoggedUserId] = useState("");
-  console.log("LINE AT 5" , post);
-const PF = process.env.REACT_APP_PUBLIC_FOLDER;
-useEffect(()=>{
-  const getCreator = async()=>{
-    const res = await axios.get(`http://localhost:5000/api/user/${post?.userId}`)
-    const user = res.data;
-    const name = user.name;
-    const id = user._id;
-    setCreator(name);
-    setLoggedUserId(id);
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { like } from "../../redux/apiCalls";
+import SendIcon from '@mui/icons-material/Send';
+import Modal from "../modal/Modal";
+const Post = ({ post }) => {
+  const localPost = useSelector((state) => state.post);
+  const {currentUser} = useSelector((state) => state.user);
+  // console.log("LINE AT 9", localPost);
+  const [creator, setCreator] = useState("");
+  const loggedUserId = currentUser._id;
+  // console.log("LINE AT 5", post);
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  useEffect(() => {
+    const getCreator = async () => {
+      try {
+        const res = await axios.get(
+          `http://localhost:5000/api/user/${post?.userId}`
+        );
+        const user = res?.data;
+        const name = user?.name;
+        // const id = user?._id;
+        setCreator(name);
+
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getCreator();
+  });
+  const dispatch = useDispatch();
+  const id = post._id;
+  const handleLike = () => {
+    like(dispatch, id, loggedUserId);
+    window.location.reload();
+  };
+
+  const [comment , setComment] = useState("");
+  const [open , setOpen] = useState(false);
+
+  const handleClick = async()=>{
+    try {
+       await axios.post("http://localhost:5000/api/comment/" , {loggedUserId , id , comment});
+      setComment("")
+    } catch (error) {
+      console.log(error);
+    }
   }
-  getCreator();
-});
-const dispatch = useDispatch();
-const id = post._id;
-const handleLike = ()=>{
-  like(dispatch , id , loggedUserId);
-  // window.location.reload();
-};
 
   return (
-    <div className='post'>
+    <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img src= {"/assets/1.jpg"} alt="" className='postProfileImg' />
-            <span className='postUsername'>{creator}</span>
+            <img src={"/assets/1.jpg"} alt="" className="postProfileImg" />
+            <span className="postUsername">{creator}</span>
           </div>
           <div className="postTopRight">
-            <MoreVertIcon/>
+            <MoreVertIcon />
           </div>
         </div>
         <div className="postCenter">
-          <span className='postText'>{post.desc}</span>
-          <img src={PF + post?.img} alt="" className='postImg' />
+          <span className="postText">{post.desc}</span>
+          <img src={post?.imgUrl} alt="" className="postImg" />
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img src="/assets/like.png" alt="" className='likeIcon' onClick={handleLike} />
-            <img src="/assets/heart.png" alt="" className='likeIcon' />
-            <span className='postLikeCounter'>{(!localPost)?post.likes:localPost?.currentPost.likes.length} peoples like it</span>
+            <img
+              src="/assets/like.png"
+              alt=""
+              className="likeIcon"
+              onClick={handleLike}
+            />
+            <span className="postLikeCounter">
+              {localPost?.currentPost?._id === id
+                ? localPost?.currentPost?.likes.length
+                : post?.likes.length}
+              peoples like it
+            </span>
           </div>
-         
+
+          <div className="postBottomCenter">
+            <span className="postBottomCenterComments" onClick={()=>setOpen(true)}>comments</span>
+            {open && <Modal setOpen={setOpen} userId={loggedUserId} postId={id}/>}
+          </div>
+
           <div className="postBottomRight">
-            <span className='postCommentText'>{post.comment} comments</span>
+            <input type="text" placeholder="comment.." value={comment} onChange={(e)=> setComment(e.target.value)}  className="postBottomRightInput"/>
+            <SendIcon style={{"color":"#1877f2"}} onClick={handleClick}/>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
